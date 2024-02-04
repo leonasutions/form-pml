cc4ca4e17ce7d2c43348d16bfcb8f2c502c9113d
leonasutions <52985333+leonasutions@users.noreply.github.com>
2024-02-05 01:45:47 +0700

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('')
+  async login(@Res() res) {
+    var path = require('path');
+    return res.sendFile(path.resolve('public/login.html'));
   }
+
+  @Get('1')
+  async inputData(@Res() res) {
+    res.render('one', { title: 'Hey', message: 'Hello there!' })
+  }
+
+
 }
